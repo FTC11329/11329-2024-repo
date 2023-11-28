6740ab2119c86082a3e2bb8a4264ce1cc8ef997f
Allen <allenthomasbrown@gmail.com>
2023-11-27 20:59:53 -0500

    Realized that i was getting like 5000 as my Vh in this. i dont know how

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/AprilTagToRoadRunner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utility/AprilTagToRoadRunner.java
@@ -3,20 +3,31 @@ package org.firstinspires.ftc.teamcode.utility;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.opencv.core.Mat;
 
 public class AprilTagToRoadRunner {
     public static Pose2d tagToRunner(AprilTagDetection tag) {
-        Pose2d tagPose = new Pose2d(tag.ftcPose.x, tag.ftcPose.y, 90 - tag.ftcPose.yaw);
+        Pose2d tagPose = new Pose2d(tag.ftcPose.x, tag.ftcPose.y, tag.ftcPose.bearing);
+        double Vx = tagPose.getX();
+        double Vy = tagPose.getY();
+        double Vh = tagPose.getHeading();
+
         //if V is vision so Vh is vision heading then this is the math in my words:
         //Rx = sqrt(Vx^2 + Vy^2) * sin(Vh + arcsin( (Vx)/(sqrt(Vx^2 + Vy^2) )
-        double runnerPoseX = Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2)) + Math.sin(tagPose.getHeading() + Math.asin(tagPose.getX()/(Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2)))));
+//        double runnerPoseX = Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2)) + Math.sin(tagPose.getHeading() + Math.asin(tagPose.getX()/(Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2)))));
 
-        //Ry = sqrt(- Rx^2 + Vx^2 + Vy^2)
-        double runnerPoseY = Math.sqrt(-Math.pow(runnerPoseX, 2) + Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2));
+        //Ry = sqrt(- Rx^2 + abs(Vx^2 + Vy^2))
+//        double runnerPoseY = Math.sqrt(-Math.pow(runnerPoseX, 2) + Math.abs(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2)));
 
         //Rh = Vh + arcsin((Vx)/(sqrt(Vx^2 + Vy^2)
-        double runnerPoseHeading = tagPose.getHeading() + Math.asin(tagPose.getX()/(Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2))));
+//        double runnerPoseHeading = tagPose.getHeading() + Math.asin(tagPose.getX()/(Math.sqrt(Math.pow(tagPose.getX(), 2) + Math.pow(tagPose.getY(), 2))));
 
-        return new Pose2d(runnerPoseX, runnerPoseY, runnerPoseHeading);
+        double Rx = (Vy - (Vx * Math.tan(Vh)) * Math.sin(Vh))/(Math.tan(Vh));
+
+        double Ry = (Vy - Vx * Math.tan(Vh) * Math.sin(Vh) + Vx * Math.tan(Vh));
+
+        double Rh = 0;
+
+        return new Pose2d(Vx, Vy, tag.ftcPose.yaw);
     }
 }
