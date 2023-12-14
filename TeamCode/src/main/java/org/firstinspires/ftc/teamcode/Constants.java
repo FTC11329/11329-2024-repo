9058acc4d47cde81e0f0ff3e7f73b96370643cbe
Allen <allenthomasbrown@gmail.com>
2023-12-13 22:11:19 -0500

    it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -130,7 +130,7 @@ public class Constants {
          */
         public static double MAX_VEL = 30;
         public static double MAX_ACCEL = 30;
-        public static double MAX_ANG_VEL = Math.toRadians(60);
-        public static double MAX_ANG_ACCEL = Math.toRadians(60);
+        public static double MAX_ANG_VEL = Math.toRadians(270);
+        public static double MAX_ANG_ACCEL = Math.toRadians(270);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Drivetrain.java
@@ -56,8 +56,8 @@ import java.util.List;
 public class Drivetrain extends MecanumDrive {
     private static final TrajectoryVelocityConstraint VEL_CONSTRAINT = getVelocityConstraint(MAX_VEL, MAX_ANG_VEL, TRACK_WIDTH);
     private static final TrajectoryAccelerationConstraint ACCEL_CONSTRAINT = getAccelerationConstraint(MAX_ACCEL);
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(6, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(6, 0, 0);
     public static double LATERAL_MULTIPLIER = 1;
     public static double VX_WEIGHT = 1;
     public static double VY_WEIGHT = 1;
